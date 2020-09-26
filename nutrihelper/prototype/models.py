cda32f4304e876f7b7978dbd9274af24543aec65
John Owens <jowens13@emich.edu>
2020-09-25 21:28:02 -0400

    Changed data types and added a link to the USDA database in readme.

--- nutrihelper/prototype/models.py
+++ nutrihelper/prototype/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Food(models.Model):
 	#id auto generated (egg is 1 broccoli is 2)
 	name = models.CharField(max_length=50)
-	servingSize = models.CharField(max_length=10)
+	servingSize = models.DecimalField(max_digits=5, decimal_places=2)
 	calories = models.IntegerField()
-	totalFat = models.CharField(max_length=10)
-	cholesterol = models.CharField(max_length=10)
-	sodium = models.CharField(max_length=10)
-	totalCarb = models.CharField(max_length=10)
-	protein = models.CharField(max_length=10)
+	totalFat = models.DecimalField(max_digits=5, decimal_places=2)
+	cholesterol = models.DecimalField(max_digits=5, decimal_places=2)
+	sodium = models.DecimalField(max_digits=5, decimal_places=2)
+	totalCarb = models.DecimalField(max_digits=5, decimal_places=2)
+	protein = models.DecimalField(max_digits=5, decimal_places=2)
 	
 	#currently just gets the name
 	def __str__(self):
