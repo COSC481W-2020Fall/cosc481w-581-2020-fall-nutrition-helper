68d1f20fbc6c2f2f022c12a1e8c7b210c4564512
a-xia <axia@emich.edu>
2020-10-04 18:59:45 -0400

    added adjustable portions functionality

--- nutrihelper/prototype/urls.py
+++ nutrihelper/prototype/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 	# /prototype/
 	path('', views.IndexView.as_view(), name='index'),
 	# /prototype/nutrifacts/
-	path('nutrifacts/', views.FactsView.as_view(), name='nutrifacts'),
+	path('nutrifacts/<int:pk>/', views.FactsView.as_view(), name='nutrifacts'),
 	# /prototype/description/
 	path('description/', views.DescriptionView.as_view(), name='description'),
     # /prototype/search/
