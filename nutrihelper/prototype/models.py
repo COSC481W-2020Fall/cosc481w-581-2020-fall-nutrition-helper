9f720e3f14e199a0367d9e57def9b58cc765f582
JalenDmarion <jhudso29@emich.edu>
2020-10-05 10:48:12 -0400

    Updated the max value from 5 digits --> 8 digits

--- nutrihelper/prototype/models.py
+++ nutrihelper/prototype/models.py
@@ -6,13 +6,13 @@ from django.db import models
 class Food(models.Model):
 	#id auto generated (egg is 1 broccoli is 2)
 	name = models.CharField(max_length=50)
-	servingSize = models.DecimalField(max_digits=5, decimal_places=2)
+	servingSize = models.DecimalField(max_digits=8, decimal_places=2)
 	calories = models.IntegerField()
-	totalFat = models.DecimalField(max_digits=5, decimal_places=2)
-	cholesterol = models.DecimalField(max_digits=5, decimal_places=2)
-	sodium = models.DecimalField(max_digits=5, decimal_places=2)
-	totalCarb = models.DecimalField(max_digits=5, decimal_places=2)
-	protein = models.DecimalField(max_digits=5, decimal_places=2)
+	totalFat = models.DecimalField(max_digits=8, decimal_places=2)
+	cholesterol = models.DecimalField(max_digits=8, decimal_places=2)
+	sodium = models.DecimalField(max_digits=8, decimal_places=2)
+	totalCarb = models.DecimalField(max_digits=8, decimal_places=2)
+	protein = models.DecimalField(max_digits=8, decimal_places=2)
 	
 	#currently just gets the name
 	def __str__(self):
