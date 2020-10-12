701c1dc6880300eac623235cb36f67d906102253
a-xia <axia@emich.edu>
2020-10-11 21:41:45 -0400

    added daily log and related models

--- project/nutrihacker/models.py
+++ project/nutrihacker/models.py
@@ -90,4 +90,25 @@ class Profile(models.Model):
 		}
 
 
+class DailyLog(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	date = models.DateField()
 
+	def __str__(self):
+		return str(self.date)
+	# TODO: add function to calculate total nutrition information whenever a meal log field is added/modified
+
+class MealLog(models.Model):
+	log_time = models.DateTimeField()
+	daily_log = models.ForeignKey(DailyLog, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.log_time)
+
+class MealFood(models.Model):
+	meal_log = models.ForeignKey(MealLog, on_delete=models.CASCADE)
+	food = models.ForeignKey(Food, on_delete=models.CASCADE)
+	quantity = models.DecimalField(max_digits=5, decimal_places=2)
+
+	def __str__(self):
+		return self.food.name
