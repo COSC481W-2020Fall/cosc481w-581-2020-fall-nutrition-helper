b1dc0e9c7201a8f8894e1082f0878df325715cbe
Michael Neet <mneet@emich.edu>
2020-10-17 04:19:41 -0400

    add allergy/diet preferences excludes choices that have already been added

--- project/nutrihacker/forms.py
+++ project/nutrihacker/forms.py
@@ -3,13 +3,27 @@ from django import forms
 from .models import Allergy, DietPreference
 from .models import DailyLog, MealLog, Food
 
-# creates a multiple choice select field to add allergies
+# creates a model choice select field to add allergies
 class AllergyChoiceForm(forms.Form):
     allergy_select = forms.ModelChoiceField(label="Add allergy", queryset=Allergy.objects.all())
+    
+    def __init__(self,*args,**kwargs):
+        self.current_profile = kwargs.pop('current_profile', None) 
+        super(AllergyChoiceForm,self).__init__(*args,**kwargs)
+        # exclude choices if they've already been added to the profile
+        if self.current_profile != None:
+            self.fields['allergy_select'].queryset = Allergy.objects.exclude(profiles=self.current_profile)
 
 # similar to AlleryChoiceForm but for diet preferences
 class DietChoiceForm(forms.Form):
     diet_select = forms.ModelChoiceField(label="Add diet preference", queryset=DietPreference.objects.all())
+    
+    def __init__(self,*args,**kwargs):
+        self.current_profile = kwargs.pop('current_profile', None)
+        super(DietChoiceForm,self).__init__(*args,**kwargs)
+        # exclude choices if they've already been added to the profile
+        if self.current_profile != None:
+            self.fields['diet_select'].queryset = DietPreference.objects.exclude(profiles=self.current_profile)
 
 class LogForm(forms.Form):
 	date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
