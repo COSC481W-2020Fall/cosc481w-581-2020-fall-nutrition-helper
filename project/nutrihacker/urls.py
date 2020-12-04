bf08629a08b9f8f59a2f57bb1acd9ebebf624f79
Tsion-Tadesse <46496704+Tsion-Tadesse@users.noreply.github.com>
2020-12-03 23:13:29 -0500

    Description page removed

--- project/nutrihacker/urls.py
+++ project/nutrihacker/urls.py
@@ -15,8 +15,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 	# /nutrihacker/nutrifacts/
     path('nutrifacts/<int:pk>/', views.FactsView.as_view(), name='nutrifacts'),
-	# /nutrihacker/description/
-    path('description/', views.DescriptionView.as_view(), name='description'),
+
     # /nutrihacker/food_autocomplete/
     path('food_autocomplete/', views.FoodAutocomplete.as_view(), name='food_autocomplete'),
     # /nutrihacker/recipe_autocomplete/
