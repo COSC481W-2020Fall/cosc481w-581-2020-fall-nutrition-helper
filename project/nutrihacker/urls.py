6cd8ad7f8b652775b41c0ba11bffd3a610cb2fb8
Tsion-Tadesse <46496704+Tsion-Tadesse@users.noreply.github.com>
2020-12-10 20:28:31 -0500

    navigation bar fixed

--- project/nutrihacker/urls.py
+++ project/nutrihacker/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django import forms 
+#from users import views as user_views
 
 from . import views
 
